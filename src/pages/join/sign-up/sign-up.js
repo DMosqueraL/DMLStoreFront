92f5e6e759fc5a2529f0b702045fcbd22e9d63e3
DMosqueraL <dml2410@gmail.com>
2023-05-20 19:53:52 -0500

    Creación de páginas de Auth

---
+++ src/pages/join/sign-up/sign-up.js
@@ -0,0 +1,11 @@
+import styles from "./sign-up.module.scss";
+
+export default function SignUpPage() {
+    return (
+        <>
+        <div>
+            <h3>Crear Cuenta</h3>
+        </div>
+        </>
+    )
+};
