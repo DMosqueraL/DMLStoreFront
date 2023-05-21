18ad5c6a548b7f9e329b94a91ceebbcef0766a7c
DMosqueraL <dml2410@gmail.com>
2023-05-21 09:32:46 -0500

    Creación del Formulario de Registro

--- src/pages/join/sign-up/sign-up.js
+++ src/pages/join/sign-up/sign-up.js
@@ -1,12 +1,18 @@
-import styles from "./sign-up.module.scss";
+import Link from "next/link";
 import { JoinLayout } from "@/layouts";
+import { RegisterForm } from "@/components/Auth";
+import styles from "./sign-up.module.scss";
 
 export default function SignUpPage() {
   return (
     <>
       <JoinLayout>
-        <div>
+        <div className={styles.signIn}>
           <h3>Crear Cuenta</h3>
+          <RegisterForm />
+          <div className={styles.actions}>
+            <Link href="/join/sign-in">Atrás</Link>
+          </div>
         </div>
       </JoinLayout>
     </>
