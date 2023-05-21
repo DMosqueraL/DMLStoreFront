36fd4b291b21f4e38bfa9b6a2aedbbe38d2ecd75
DMosqueraL <dml2410@gmail.com>
2023-05-21 10:16:15 -0500

    Control y validación de datos del formulario de registro

---
+++ src/components/Auth/ResgisterForm/RegisterForm.js
@@ -0,0 +1,61 @@
+import { Form } from "semantic-ui-react";
+import { useFormik } from "formik";
+import { inicialValues, validationSchema } from "./RegisterForm.form";
+
+export function RegisterForm() {
+  const formik = useFormik({
+    initialValues: inicialValues(),
+    validationSchema: validationSchema(),
+    validationOnChange: false,
+    onSubmit: (formValue) => {
+      console.log("Formulario Enviado");
+      console.log(formValue)
+    },
+  });
+
+  return (
+    <Form onSubmit={formik.handleSubmit}>
+      <Form.Group widths="equal">
+        <Form.Input
+          name="email"
+          type="text"
+          placeholder="Correo Electrónico"
+          values={formik.values.email}
+          onChange={formik.handleChange}
+          error={formik.errors.email}
+        />
+        <Form.Input
+          name="username"
+          type="text"
+          placeholder="Nombre de Usuario"
+          values={formik.values.username}
+          onChange={formik.handleChange}
+          error={formik.errors.username}
+        />
+      </Form.Group>
+
+      <Form.Group widths="equal">
+        <Form.Input
+          name="name"
+          type="text"
+          placeholder="Nombres y Apellidos"
+          values={formik.values.name}
+          onChange={formik.handleChange}
+          error={formik.errors.name}
+        />
+        <Form.Input
+          name="password"
+          type="password"
+          placeholder="Contraseña"
+          values={formik.values.password}
+          onChange={formik.handleChange}
+          error={formik.errors.password}
+        />
+      </Form.Group>
+
+      <Form.Button type="submit" fluid loading={formik.isSubmitting}>
+        Registrarse
+      </Form.Button>
+    </Form>
+  );
+}
--- src/components/Auth/index.js
+++ src/components/Auth/index.js
@@ -1,1 +1,1 @@
-export * from "./ResgisterFeorm";
+export * from "./ResgisterForm";
