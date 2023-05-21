200a94e4e332a96c7e0ce5cf03d433b60d4d155f
DMosqueraL <dml2410@gmail.com>
2023-05-20 19:05:25 -0500

    Modificando estilos a componentes de Semantic UI

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,14 @@
 import React from 'react'
+import { Button } from "semantic-ui-react";
 
 export default function Index() {
   return (
     <div>
-      <h2>DML Store</h2>      
+      <h2>DML Store</h2>
+      <br></br>    
+      <Button primary>
+        Ir al inicio
+      </Button>
     </div>
   );
 }
