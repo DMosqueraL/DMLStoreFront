9570378dabee948e40ff3ba41b7e47aac68107a6
DMosqueraL <dml2410@gmail.com>
2023-05-30 08:07:29 -0500

    Se implementa la parte inicial de la página Nosotros

---
+++ src/pages/nosotros/nosotros.js
@@ -0,0 +1,12 @@
+import { BasicLayout } from "@/layouts";
+import styles from "./nosotros.module.scss";
+
+export default function NosotrosPage() {
+  return (
+    <>
+    <BasicLayout relative>
+      <h2>Nuestra Empresa</h2>
+    </BasicLayout>
+    </>
+  );
+}
